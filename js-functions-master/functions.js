321a09c20b67f502eb89234b441193160c8c02a3
trung dong <trungdongchiwchew@gmail.com>
2022-03-19 15:00:31 +0700

    first commit

---
+++ js-functions-master/functions.js
@@ -0,0 +1,281 @@
+/**
+ * Converts a number a string.
+ * @param {number} n
+ * @return {string} the number as a string
+ */
+
+function numberToString (n){   
+    var stringNum = n.toString();
+    return stringNum;
+}
+
+/**
+ * Adds one to a given number.
+ * @param {number} n
+ * @return {number}
+ */
+ function increase(n) {
+	n = n + 1;
+	return n;
+}
+    function decrease(n) {
+        n = n - 1;
+        return n;
+    }
+
+/**
+ * Subtracts one from a given number.
+ * @param {number} n
+ * @return {number}
+ */
+    function add(x, y) {
+        var sum = x + y;
+        return sum;
+    }
+
+    
+/**
+ * Adds two numbers.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the sum
+ */
+    
+
+/**
+ * Subtracts the second number from the first.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the difference
+ */
+    function subtract(x, y) {
+        var diff = x - y;
+        return diff;
+    }
+    function divide(x, y) {
+        var divi = x / y;
+        return divi;
+    }
+
+/**
+ * Multiplies two numbers.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the product
+ */
+    function multiply(x, y) {
+        var by = x * y;
+        return by;
+    }
+
+
+/**
+ * Divides the first number by the second.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the quotient
+ */
+
+    function divide(x, y) {
+        var divi = x / y;
+        return divi;
+    }
+
+
+/**
+ * Multiplies a number by itself.
+ * @param {number} x, number to be squared
+ * @return {number} squared
+ */
+    function square(x) {
+        var squ = x * x;
+        return squ;
+    }
+
+
+/**
+ * Performs a mathematical operation on two numbers.
+ * Also prints out the equation: (i.e.) "1 + 5 = 6" or "8 / 2 = 4".
+ * @param {string} operation "add", "subtract", "multiply", or "divide"
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the result
+ */
+    function calculate(operation, x, y) {
+        var total = 0;
+
+        if (operation === 'add'){
+            total = x + y;
+            console.log (x + ' + ' + y +' = '  + total);
+
+        }else if (operation === 'subtract'){
+            total = x - y;
+            console.log (x + ' - ' + y + ' = ' + total);
+
+        }
+        else if (operation === 'multiply'){
+            total = x * y;
+            console.log (x + ' * ' + y + ' = ' + total);
+
+        }else if (operation === 'divide'){
+            total = x / y;
+            console.log (x + ' / ' + y +' = ' + total);
+
+        }
+        return total;
+    }
+
+
+/**
+ * Returns true if `a` is greater than `b`.
+ * @param {number} a
+ * @param {number} b
+ * @return {boolean} `a` is larger than `b`
+ */
+    function isGreaterThan (a, b) {
+        return a > b; 
+    }
+
+/**
+ * Returns true if `a` is less than `b`.
+ * @param {number} a
+ * @param {number} b
+ * @return {boolean} `a` is smaller than `b`
+ */
+    function isLessThan (a, b) {
+        return a < b; 
+    }
+
+/**
+ * Returns true if `a` and `b` are equal.
+ * @param {number} a
+ * @param {number} b
+ * @return {boolean} the numbers are equal
+ */
+    function areEqual (a, b) {
+        return a === b;
+    }
+
+/**
+ * Returns the smallest value of two numbers.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the smallest number
+ */
+    function minimum (x, y) {
+        return Math.min(x, y);
+    }
+
+/**
+ * Returns the largest value of two numbers.
+ * @param {number} x
+ * @param {number} y
+ * @return {number} the largest number
+ */
+    function maximum (x, y) {
+        return Math.max(x, y);
+    }
+
+/**
+ * Returns true if `n` is even.
+ * @param {number} n
+ * @return {boolean} the number is even
+ */
+    function isEven(n) {
+        return n % 2 === 0;
+    }
+
+/**
+ * Returns true if `n` is odd.
+ * @param {number} n
+ * @return {boolean} the number is odd
+ */
+    function isOdd(n) {
+        return n % 2 !== 0;
+    }
+
+
+/**
+ * Returns a letter grade.
+ * "A": 90-100%
+ * "B": 80-89%
+ * "C": 70-79%
+ * "D": 60-69%
+ * "F": 0-59%
+ * @param {number} score
+ * @param {number} total maximum possible score
+ * @return {string} the score represented as a letter grade
+ */
+    function letterGrade(x, y){
+
+        var grade = divide (x, y);
+        if (grade >= .9){
+
+        if (grade >= .9) {
+            return 'A';
+        }
+        }else if (grade >= .8) {
+            return 'B';
+        }
+
+        else if (grade >= .7) {
+            return 'C';
+        }
+
+        else if (grade >= .6) {
+            return 'D';
+
+        }else{
+            return 'F';
+
+        }
+    }
+
+
+/**
+ * Checks if a `restaurant` object has a `reviews` property.
+ * If it does, increase the property's `reviews` value by 1.
+ * If it does not, set the `reviews` value to 1.
+ * @param {object} restaurant   represents a restaurant object
+ * @return {object} restaurant
+ */
+    function incrementReviews(x) {
+
+        if (x.reviews) {
+            x.reviews ++;
+
+        }else{
+            x.reviews = 1;
+
+        }
+
+        return x;
+    }
+
+/**
+ * Joins two strings with a space.
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string} joined the words joined with a space
+ */
+    function combine(word1, word2) {
+        return word1 + ' ' + word2;
+    }
+
+/**
+ * Returns a circle object with the properties `circumference` and `area`.
+ * Use Math.PI for the value π.
+ * (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/PI)
+ * @param {number} radius
+ * @return {object} circle
+ */
+    function createCircle(radius) {
+        var radius = 4;
+        var circle = {
+            area: Math.PI (radius * radius),
+            circumference: Math.PI * 2 * radius
+        }
+
+        return circle;
+    }       
+
